7d7754d65a57c6f46ae2f5a3cb095c28d4e7af29
maldo <maldo@LAPTOP-PPPO3N3Q>
2023-05-04 09:47:09 -0600

    otros cambios

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -28,13 +28,56 @@ namespace Laboratorio__11
         protected void Button2_Click(object sender, EventArgs e)
         {
             List<Universidad> u = new List<Universidad>();
-            
-            
+
+            u = Leer();
+
+            if (u == null) u = new List<Universidad>();
+
+            Universidad uni = u.Find(a => a.Uni == DropDownList1.SelectedValue);
+
+            if (uni == null)
+            {
+                Universidad uniN = new Universidad();
+
+                uniN.Uni = DropDownList1.SelectedValue;
+
+                Alumnos al = new Alumnos();
+                al.NC = TextBox1.Text;
+                al.Nombre = TextBox2.Text;
+                al.Dir = TextBox3.Text;
+                al.Fn = TextBox4.Text;
+
+                //crear el nuevo curso
+                Curso cu = new Curso();
+                cu.Nombre = TextBox5.Text;
+                cu.Nota = Convert.ToInt32(TextBox6.Text);
+
+                al.C.Add(cu);
+
+                uniN.Al.Add(al);
+                u.Add(uniN);
+            }
+
+            Grabar(u);
         }
 
-        private void leer()
+        private List<Universidad> Leer()
         {
+            List<Universidad> lista = new List<Universidad>();
+            string archivo = Server.MapPath("Datos.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            lista = JsonConvert.DeserializeObject<List<Universidad>>(json);
 
+            return lista;
+        }
+
+        private void Grabar(List<Universidad> universidades)
+        {
+            string json = JsonConvert.SerializeObject(universidades);
+            string archivo = Server.MapPath("Datos.json");
+            System.IO.File.WriteAllText(archivo, json);
         }
     }
 }
